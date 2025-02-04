c5d404760caf3b007473023349c241ec1f33a8cf
eno <k1990121@gmail.com>
2025-02-05 01:39:34 +0900

    pk 이름 idx로 통일

--- src/auth/guard/jwt.strategy.ts
+++ src/auth/guard/jwt.strategy.ts
@@ -16,7 +16,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   async validate(payload: any) {
     console.log('payload', payload);
     return {
-      userIdx: payload.sub,
+      idx: payload.idx,
       userId: payload.user_id,
       nickname: payload.nickname,
     };
