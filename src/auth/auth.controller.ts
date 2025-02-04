645ad3446c4b7b92228e20564acd3e1bae08b260
eno <k1990121@gmail.com>
2025-02-05 01:40:17 +0900

    가칭 본인인증시 stream, ivs row 추가 및 swagger jwt 추가

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,15 @@ import { Controller, Request, Post, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from './guard/local-auth.guard';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './guard/jwt-auth.guard';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiResponse,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { LoginResponse } from './entities/login.response';
+import { AuthEntity, AuthFailResponse } from './entities/login.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -13,6 +22,17 @@ export class AuthController {
    * @returns access_token에 jwt를 담아서 반납(idx, user_id, nickname 정보를 담음)
    */
   @Post('login')
+  @ApiOperation({ summary: '로그인' })
+  @ApiResponse({
+    status: 201,
+    description: '로그인 성공',
+    type: LoginResponse,
+  })
+  @ApiUnauthorizedResponse({
+    description: '로그인 실패',
+    type: AuthFailResponse,
+  })
+  @ApiBody({ description: 'Body 데이터', type: AuthEntity, required: true })
   @UseGuards(LocalAuthGuard)
   async login(@Request() req) {
     return this.authService.login(req.user);
@@ -22,8 +42,11 @@ export class AuthController {
    * 휴대폰 본인 인증 경로(가칭)
    */
   @Post('phone-verification')
+  @ApiOperation({ summary: '가칭. 본인인증 성공했을시 stream, ivs 만드는용도' })
+  @ApiBearerAuth()
   @UseGuards(JwtAuthGuard)
   async phoneVerification(@Request() req) {
+    console.log(req.user);
     return await this.authService.verifyPhone(req.user);
   }
 }
--- src/oauth/oauth.controller.ts
+++ src/oauth/oauth.controller.ts
@@ -1,17 +1,23 @@
 import { Controller, Get, Request, Res } from '@nestjs/common';
 
 import { OauthService } from './oauth.service';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('oauth')
 export class OauthController {
   constructor(private readonly oauthService: OauthService) {}
 
   @Get('google/login')
+  @ApiOperation({ summary: '구글 Oauth 로그인 요청 URL' })
   async googleLogin(@Res() res) {
     res.redirect(this.oauthService.getGoogleLoginURL());
   }
 
   @Get('google/callback')
+  @ApiOperation({
+    summary: '구글 Oauth 콜백 주소',
+    description: 'jwt쿠키 세팅 후 redirect',
+  })
   async googleCallback(@Request() req, @Res() res) {
     const code = req.query.code as string;
     const jwt = await this.oauthService.getGoogleLoginJwt(code);
