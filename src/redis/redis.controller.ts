ae338f69f81e4ec1f386678edddb44eb2b60e9b3
eno <k1990121@gmail.com>
2025-02-08 07:47:50 +0900

    a

--- src/redis/redis.controller.ts
+++ src/redis/redis.controller.ts
@@ -1,19 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { RedisService } from './redis.service';
-import { InjectRedis } from '@nestjs-modules/ioredis';
-import Redis from 'ioredis';
+
 import { MessagePattern } from '@nestjs/microservices';
 
 @Controller('redis')
 export class RedisController {
-  constructor(
-    private readonly redisService: RedisService,
-    @InjectRedis() private readonly redis: Redis,
-  ) {}
+  constructor(private readonly redisService: RedisService) {}
 
   @Get('test')
   async test() {
-    await this.redis.publish('test', 'Hello Redis!');
     return 'Published!';
   }
 
--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -1,6 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRedis } from '@nestjs-modules/ioredis';
+import Redis from 'ioredis';
 
 @Injectable()
 export class RedisService {
-  constructor() {}
+  constructor(@InjectRedis() private readonly redis: Redis) {}
+
+  async publicshMessage() {
+    await this.redis.publish('test', 'Hello Redis!');
+  }
 }
