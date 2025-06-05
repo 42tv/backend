7a1b99b6ded1373fd901956bede7d93538415028
eno <k1990121@gamil.com>
2025-06-06 03:39:50 +0900

    play API 호출  시 bookmark_cnt 추가

--- src/play/play.module.ts
+++ src/play/play.module.ts
@@ -5,9 +5,10 @@ import { UserModule } from 'src/user/user.module';
 import { StreamModule } from 'src/stream/stream.module';
 import { AuthModule } from 'src/auth/auth.module';
 import { BlacklistModule } from 'src/blacklist/blacklist.module';
+import { BookmarkModule } from 'src/bookmark/bookmark.module';
 
 @Module({
-  imports: [UserModule, StreamModule, AuthModule, BlacklistModule],
+  imports: [UserModule, StreamModule, AuthModule, BlacklistModule, BookmarkModule],
   controllers: [PlayController],
   providers: [PlayService],
   exports: [PlayService],
