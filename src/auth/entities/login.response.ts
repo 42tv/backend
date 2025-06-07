ef7423463eafd4ff9ace4fdfa25fbfcfc37a35fa
eno <k1990121@gamil.com>
2025-06-07 11:04:49 +0900

    깃가디언에 탐지되서 제거

--- src/auth/entities/login.response.ts
+++ src/auth/entities/login.response.ts
@@ -4,13 +4,13 @@ export class LoginResponse {
   @ApiProperty({
     description: 'access_token 예제에 있는건 테스트용 999일짜리',
     example:
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZHgiOjE0LCJ1c2VyX2lkIjoidXNlcjEyMyIsIm5pY2tuYW1lIjoibmlja25hbWUxMjMiLCJpYXQiOjE3MzkxMTE4ODIsImV4cCI6MTgyNTQyNTQ4Mn0.aedkaSuPjdgRjgiQgyzxKTPHrDw-rIyudYuiW3zG1wg',
+      'something jwt',
   })
   access_token: string;
   @ApiProperty({
     description: 'refresh_token 예제에 있는건 테스트용 999일짜리',
     example:
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZHgiOjE0LCJ1c2VyX2lkIjoidXNlcjEyMyIsIm5pY2tuYW1lIjoibmlja25hbWUxMjMiLCJpYXQiOjE3MzkxMTE4ODIsImV4cCI6MTgyNTQyNTQ4Mn0.iDCiW3IccIgbh4VZMv1fwEibXs5AGBRbOt0VFSKCiTw',
+      'something jwt',
   })
   refresh_token: string;
 }
