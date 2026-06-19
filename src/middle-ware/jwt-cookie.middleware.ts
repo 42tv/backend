4c58c1d02a1463aafca5a56b36333f697f776d94
eno <k1990121@gamil.com>
2026-06-20 04:02:31 +0900

    refresh 하는거 정확하게 하기 위해 변경

--- src/middle-ware/jwt-cookie.middleware.ts
+++ src/middle-ware/jwt-cookie.middleware.ts
@@ -4,11 +4,12 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class JwtCookieMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
+    const path = req.originalUrl.split('?')[0];
+
     if (!req.headers.authorization && req.cookies?.jwt) {
       req.headers.authorization = `Bearer ${req.cookies.jwt}`;
     }
-    if (req.originalUrl.includes('/refresh') && req.cookies?.refresh) {
-      console.log('refresh');
+    if (path.endsWith('/auth/refresh') && req.cookies?.refresh) {
       req.headers.authorization = `Bearer ${req.cookies.refresh}`;
     }
 
